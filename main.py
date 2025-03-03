d3cff44d8a241ca191d068944711bc7459ea0a82
munggonegg <natthaphong.pim@ascendcorp.com>
2025-03-03 09:42:40 +0000

    Improve main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from retry import retry
 from openai import OpenAI
 from dotenv import load_dotenv
 from prompt import system_prompt
@@ -12,6 +13,7 @@ llm = OpenAI(
    base_url='https://api.opentyphoon.ai/v1'
 )
 
+@retry(tries=3, delay=2, backoff=2)
 def classify_language(user_input):
     response = llm.chat.completions.create(
         model="typhoon-v2-70b-instruct",
@@ -27,7 +29,9 @@ def classify_language(user_input):
         ]
     )
 
-    return response.choices[0].message.content.strip()
+    response = response.choices[0].message.content.strip()
+
+    return int(response)
 
 print("Language Classifier (Type 'q' to quit)")
 print("English: 1, Thai: 2, Other: 0")
@@ -37,4 +41,4 @@ while True:
         print("Goodbye!")
         break
     response = classify_language(user_input)
-    print(int(response))
+    print(response)
